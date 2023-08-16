e85485d63a5e97fe30aee877c1ba18aaa92e7864
alexwachirakimotho <lexkimothowachira@gmail.com>
2023-08-16 20:10:46 +0300

    dependency update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5d3345a40638fe1e"; }
+/******/ 		__webpack_require__.h = function() { return "cbc3b1f3207bb382"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/tour.jsx
+++ pages/tour.jsx
@@ -5,67 +5,9 @@ import Link from "next/link";
 const Tour = () => {
   return (
     <Layout extraClass={"pt-160"}>
-      <PageBanner pageTitle={"Explore Tour Place"} />
+      <PageBanner pageTitle={"Explore with us"} />
       {/*====== Start Booking Section ======*/}
-      <section className="booking-form-section pb-100">
-        <div className="container-fluid">
-          <div className="booking-form-wrapper p-r z-2">
-            <form className="booking-form-two">
-              <div className="form_group">
-                <span>Check In</span>
-                <label>
-                  <i className="far fa-calendar-alt" />
-                </label>
-                <input
-                  type="text"
-                  className="form_control datepicker"
-                  placeholder="Check In"
-                />
-              </div>
-              <div className="form_group">
-                <span>Check Out</span>
-                <label>
-                  <i className="far fa-calendar-alt" />
-                </label>
-                <input
-                  type="text"
-                  className="form_control datepicker"
-                  placeholder="Check Out"
-                />
-              </div>
-              <div className="form_group">
-                <span>Guest</span>
-                <label>
-                  <i className="far fa-user-alt" />
-                </label>
-                <input
-                  type="text"
-                  className="form_control"
-                  placeholder="Guest"
-                  name="text"
-                />
-              </div>
-              <div className="form_group">
-                <span>Accommodations</span>
-                <select className="wide">
-                  <option data-display="Accommodations">Accommodations</option>
-                  <option value={1}>Classic Tent</option>
-                  <option value={1}>Forest Camping</option>
-                  <option value={1}>Small Trailer</option>
-                  <option value={1}>Tree House Tent</option>
-                  <option value={1}>Tent Camping</option>
-                  <option value={1}>Couple Tent</option>
-                </select>
-              </div>
-              <div className="form_group">
-                <button className="booking-btn">
-                  Check Availability <i className="far fa-angle-double-right" />
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </section>
+
       {/*====== End Booking Section ======*/}
       {/*====== Start Places Section ======*/}
       <section className="places-section pb-100">
