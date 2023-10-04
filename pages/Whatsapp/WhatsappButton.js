ffbcc3c913669e4651ebba95160b43cb8b87916e
alexwachirakimotho <lexkimothowachira@gmail.com>
2023-10-04 08:24:51 +0300

    whatsapp button issue

--- pages/Whatsapp/WhatsappButton.js
+++ pages/Whatsapp/WhatsappButton.js
@@ -1,19 +1,36 @@
-// WhatsAppButton.js
-import React, { Component } from 'react';
-// import './WhatsAppButton.css'; // Import the CSS
+import React from 'react';
 
-class WhatsAppButton extends Component {
-  handleWhatsAppClick = () => {
+const WhatsAppButton = () => {
+  const handleWhatsAppClick = () => {
     window.open('https://wa.link/1qezrk', '_blank');
   };
 
-  render() {
-    return (
-      <div className="whatsapp-button" onClick={this.handleWhatsAppClick}>
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/WhatsApp_icon.png/479px-WhatsApp_icon.png" alt="WhatsApp Icon" />
-      </div>
-    );
-  }
-}
+  const buttonStyle = {
+    position: 'fixed',
+    bottom: '20px',
+    right: '20px',
+    cursor: 'pointer',
+    backgroundColor: '#25d366',
+    borderRadius: '50%',
+    padding: '10px',
+    boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
+    transition: 'background-color 0.3s',
+  };
+
+  const iconStyle = {
+    width: '30px',
+    height: '30px',
+  };
+
+  return (
+    <div style={buttonStyle} onClick={handleWhatsAppClick}>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/WhatsApp_icon.png/479px-WhatsApp_icon.png"
+        alt="WhatsApp Icon"
+        style={iconStyle}
+      />
+    </div>
+  );
+};
 
 export default WhatsAppButton;
